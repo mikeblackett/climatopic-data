b7341eec6333f4f64b8be22fa4efc765bee42674
Mike Blackett <27811528+mikeblackett@users.noreply.github.com>
2025-08-14 11:40:24 +0100

    update exports

--- src/climatopic_data/shared/io/__init__.py
+++ src/climatopic_data/shared/io/__init__.py
@@ -1,3 +1,5 @@
 """IO managers, external resources, file/system helpers"""
 
 from .thredds_resource import TDSResource
+from .dask_resource import DaskResource
+from .xarray_io_manager import XarrayIOManager
