e6c6bbe59302e879d433076a91b4b6c7d094f92b
Mike Blackett <27811528+mikeblackett@users.noreply.github.com>
2025-08-14 11:37:33 +0100

    comments/docs

--- src/climatopic_data/shared/io/dask_resource.py
+++ src/climatopic_data/shared/io/dask_resource.py
@@ -1,19 +1,22 @@
+"""Resource for sharing a Dask LocalCluster across components."""
+
 import dagster as dg
 from distributed.client import Client
 
 
 class DaskResource(dg.ConfigurableResource):
+    """Resource for sharing a Dask LocalCluster across components.
+
+    Args:
+        processes (bool, optional): Whether to use processes (True) or threads (False). Defaults to True.
+        n_workers (int | None, optional): The number of workers to start.
+        threads_per_worker (int | None, optional): The number of threads to use per worker.
+    """
+
     processes: bool = True
-    """Whether to use processes (True) or threads (False). Defaults to True."""
-
     n_workers: int | None = None
-    """The number of workers to start."""
-
-    # memory_limit: str | float | Literal['auto'] = 'auto'
-    # """The amount of memory to use *per worker*."""
-
     threads_per_worker: int | None = None
-    """The number of threads to use per worker."""
+    # memory_limit: str | float | Literal['auto'] = 'auto'
 
     def setup_for_execution(self, context: dg.InitResourceContext) -> None:
         self._client = Client(
@@ -22,12 +25,15 @@ class DaskResource(dg.ConfigurableResource):
             processes=self.processes,
             threads_per_worker=self.threads_per_worker,
         )
-        context.log.info(f'Dask dashboard: {self._client.dashboard_link}')
-        context.log.info(self._client)
+        context.log.info(
+            f'Dask LocalCluster {self._client}. '
+            f'Dashboard: {self._client.dashboard_link}'
+        )
 
     def teardown_after_execution(self, context):
         self._client.close()
 
     @property
     def client(self) -> Client:
+        """Dask LocalCluster."""
         return self._client
